e73a4d9f7213e9f3c31b7bfd4ba62659b0d19dfe
diogo <eudiogo@sp.senai.br>
2021-11-12 17:23:47 -0300

    Commit Inicial

--- src/br/projeto/site/meumenu/controller/IndexController.java
+++ src/br/projeto/site/meumenu/controller/IndexController.java
@@ -1,38 +1,151 @@
 package br.projeto.site.meumenu.controller;
 
+import javax.swing.JOptionPane;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class IndexController {
-	private TextField tfTarefa;
-	private TextField tfArea;
-	
+
+    @FXML
+    private DatePicker dpData;
+
+    @FXML
+    private TextField tfTarefa;
+
+    @FXML
+    private TextField tfStatus;
+
+    @FXML
+    private TextArea tfComentario;
+
+    @FXML
+    private Button adiar;
+
+    @FXML
+    private Button salvar;
+
+    @FXML
+    private Button apagar;
+
+    @FXML
+    private Button concluir;
+
+    @FXML
+    private Button limpar;
+    
+    
+
+    public DatePicker getDpData() {
+		return dpData;
+	}
+
+	public void setDpData(DatePicker dpData) {
+		this.dpData = dpData;
+	}
+
 	public TextField getTfTarefa() {
 		return tfTarefa;
 	}
-	public void setTfTarefas(TextField tfTarefa) {
+
+	public void setTfTarefa(TextField tfTarefa) {
 		this.tfTarefa = tfTarefa;
 	}
-	public TextField getTfArea() {
-		return tfArea;
+
+	public TextField getTfStatus() {
+		return tfStatus;
 	}
-	public void setTfArea(TextField tfArea) {
-		this.tfArea = tfArea;
+
+	public void setTfStatus(TextField tfStatus) {
+		this.tfStatus = tfStatus;
 	}
-	
-	
-	public void btSalvar() {
-		
+
+	public TextArea getTfComentario() {
+		return tfComentario;
 	}
-	public void btEditar() {
-		
+
+	public void setTfComentario(TextArea tfComentario) {
+		this.tfComentario = tfComentario;
 	}
-	public void btExcluir() {
-		
+
+	public Button getAdiar() {
+		return adiar;
 	}
-	public void btConcluir() {
-		
+
+	public void setAdiar(Button adiar) {
+		this.adiar = adiar;
 	}
-	public void btLimpar() {
-		
+
+	public Button getSalvar() {
+		return salvar;
 	}
+
+	public void setSalvar(Button salvar) {
+		this.salvar = salvar;
+	}
+
+	public Button getApagar() {
+		return apagar;
+	}
+
+	public void setApagar(Button apagar) {
+		this.apagar = apagar;
+	}
+
+	public Button getConcluir() {
+		return concluir;
+	}
+
+	public void setConcluir(Button concluir) {
+		this.concluir = concluir;
+	}
+
+	public Button getLimpar() {
+		return limpar;
+	}
+
+	public void setLimpar(Button limpar) {
+		this.limpar = limpar;
+	}
+
+	@FXML
+    public void btAdiar() {
+
+    }
+
+    @FXML
+    public void btConcluir() {
+
+    }
+
+    @FXML
+    public void btExcluir() {
+
+    }
+
+    @FXML
+    public void btLimpar() {
+
+    }
+
+    @FXML
+    public void btSalvar() {
+    	//validação de campos
+    	if(dpData.getValue()== null) {
+    		JOptionPane.showMessageDialog(null, "Informe a data Limite","informe", JOptionPane.ERROR_MESSAGE);
+    		dpData.requestFocus();
+    		
+    	}else if(tfTarefa.getText().isEmpty()) {
+    		JOptionPane.showMessageDialog(null, "Informe a Descrição da Tarefa","Informe",JOptionPane.ERROR_MESSAGE);
+    		tfTarefa.requestFocus();
+    		
+    	}else if(tfTarefa.getText().isEmpty()) {
+    		JOptionPane.showMessageDialog(null, "Informe os Comentários da Tarefa","Informe",JOptionPane.ERROR_MESSAGE);
+    		tfComentario.requestFocus();
+    	}
+    }
+
 }
